89ca202bcf4aee9029390941ba9e0b19546ba172
olofwallin <olof.wallin@gmail.com>
2018-04-15 21:41:19 +0200

    added environment variables

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var mockSlPlan = require('./mock-slplan.js');
 const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+}); 
 
 var bot = module.exports = new builder.UniversalBot(connector, function (session) {
     session.send("Hello there, and welcome to the first OBot!");
